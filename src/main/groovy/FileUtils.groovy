1bf7080e8bfbb604855ec55ee6993028bb5c20c8
Michel Blavin <michel@blavin.fr>
2011-03-29 13:25:40 +0200

    Added a method to create a timestamped filename. Added some Date utility methods.

--- src/main/groovy/FileUtils.groovy
+++ src/main/groovy/FileUtils.groovy
@@ -30,3 +30,23 @@ def static emptyDirectory(File directory, verbose = true){
 		println " -- Done."
 	} else println   " -- '$directory' doesn't exists."
 }
+
+/**
+* @param filename file of the original file
+* @return the filename with a timestamp.
+*/
+def static createTimeStampedFilename(String filename) {
+   def df = new java.text.SimpleDateFormat("yyyyMMdd-hhmmss")
+
+   String basename = filename
+   String extension = ""
+
+   def positionOftheLastPoint = filename.lastIndexOf ('.')
+   
+   if (positionOftheLastPoint > 0) {
+	   // the file has an extension.
+	   basename = filename.substring (0, positionOftheLastPoint)
+	   extension = "." + filename.substring (positionOftheLastPoint + 1)
+   }
+   return  basename + "-" + df.format (new Date()) + extension
+}
