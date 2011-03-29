363f540ea4b3a2db48b448d5c45269cd6fe46ee3
Michel Blavin <michel@blavin.fr>
2011-03-29 14:53:51 +0200

    added method to execute commands.

--- src/main/groovy/ConsoleUtils.groovy
+++ src/main/groovy/ConsoleUtils.groovy
@@ -10,3 +10,32 @@ static def waitBeforeClosing(int wait = 5, boolean verbose = true){
 	if (verbose) println "Waiting for $wait seconds."
 	Thread.sleep wait * 1000
 }
+
+/**
+ * 
+ * @param filePath path of the file containing the list of commands
+ * 
+ */
+static def runCommands (String filePath){
+	runCommands(new File (filePath))
+}
+
+/**
+ * Excecute all the line in this file. 
+ *
+ * @param file File containing the list of commands
+ */
+static def runCommands (File file){
+	file.eachLine { line -> executeCommand(line) }
+}
+
+/**
+ * Execute a command and displays the result on the console.  
+ * 
+ * @param command command to execute. 
+ */
+static def executeCommand(String command){
+	println "Running $command"
+	def proc = command.execute()
+	println proc.text
+}
