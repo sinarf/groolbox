176e04b59066ed62a7ba2a929606e43818416c44
Michel Blavin <michel@blavin.fr>
2011-03-29 12:36:25 +0200

    Added a script to clean up temp directory.

---
+++ src/main/groovy/utils/os/CleanTempDir.groovy
@@ -0,0 +1,28 @@
+package utils.os
+/**
+ * Clean up the temp directories. 
+ * Should work on any OS but I only use it on windows since this system "manage" to slow down when temp directory are to crowded. 
+ * 
+ * @author Michel Blavin
+ *
+ */
+def ant = new AntBuilder()
+def tempDirectories = []
+
+wipeDirectory System.getenv("TEMP")
+wipeDirectory System.getenv("TMP")
+
+def wipeDirectory(String directory){
+	if (directory) {
+		if (new File(directory)?.exists()) {
+			print "Wipe '$directory'"
+			AntBuilder ant = new AntBuilder()
+			ant.delete ( verbose: true, failonerror: false){
+				ant.fileset(dir: directory, includes: "/**")
+			}
+			println " -- Done."
+		} else println   "'$directory' doesn't exists, maybe you should check your configuration."
+	}
+}
+
+Thread.sleep 5000 // wait 5s before closing the window.
