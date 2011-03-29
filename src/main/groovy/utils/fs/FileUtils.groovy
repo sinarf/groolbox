5a31243b3906025440f623056e3c3df31d30929c
Michel Blavin <michel@blavin.fr>
2011-03-29 13:01:01 +0200

    externalize the methods to empty a directory.

---
+++ src/main/groovy/utils/fs/FileUtils.groovy
@@ -0,0 +1,32 @@
+package utils.fs
+/**
+ * @author sinarf
+ */
+
+
+/**
+ * empty the directory, if exists.
+ * 
+ * @param directory directory to wipe.
+ * @param verbose if true, turn the verbose flag to true on the antbuilder.
+ */
+def static emptyDirectory(String directory, verbose = true) {
+	if (directory) emptyDirectory new File(directory), verbose
+}
+
+/**
+ * empty the directory, if exists. 
+ *
+ * @param directory directory to wipe.
+ * @param verbose if true, turn the verbose flag to true on the antbuilder.
+ */
+def static emptyDirectory(File directory, verbose = true){
+	if (directory?.exists()) {
+		print "Empty '$directory'"
+		AntBuilder ant = new AntBuilder()
+		ant.delete ( verbose: verbose, failonerror: false){
+			ant.fileset(dir: directory, includes: "/**")
+		}
+		println " -- Done."
+	} else println   " -- '$directory' doesn't exists."
+}
--- src/main/groovy/utils/os/CleanTempDir.groovy
+++ src/main/groovy/utils/os/CleanTempDir.groovy
@@ -1,28 +1,16 @@
 package utils.os
+
+import utils.fs.FileUtils
+
 /**
- * Clean up the temp directories. 
+ * Clean up the temporary directories. 
  * Should work on any OS but I only use it on windows since this system "manage" to slow down when temp directory are to crowded. 
  * 
- * @author Michel Blavin
+ * @author sinarf
  *
  */
-def ant = new AntBuilder()
-def tempDirectories = []
 
-wipeDirectory System.getenv("TEMP")
-wipeDirectory System.getenv("TMP")
-
-def wipeDirectory(String directory){
-	if (directory) {
-		if (new File(directory)?.exists()) {
-			print "Wipe '$directory'"
-			AntBuilder ant = new AntBuilder()
-			ant.delete ( verbose: true, failonerror: false){
-				ant.fileset(dir: directory, includes: "/**")
-			}
-			println " -- Done."
-		} else println   "'$directory' doesn't exists, maybe you should check your configuration."
-	}
-}
+FileUtils.emptyDirectory System.getenv("TEMP")
+FileUtils.emptyDirectory System.getenv("TMP")
 
 Thread.sleep 5000 // wait 5s before closing the window.
