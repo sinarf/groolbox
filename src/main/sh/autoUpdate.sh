6043d490f20ae3b889106895a31c1207b1f7f5f7
Michel Blavin <michel@blavin.fr>
2011-11-06 14:53:32 +0100

    move to xubuntu.

--- src/main/sh/autoUpdate.sh
+++ src/main/sh/autoUpdate.sh
@@ -14,10 +14,11 @@ echo "Update everything"
 sudo apt-get -y -qq update &&\
 sudo apt-get -y -qq upgrade &&\
 
+echo "Install software"
+sudo apt-get install -y -qq gawk maven2 groovy rar git git-gui openssh-server lm-sensors lame gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla tasque  xubuntu-restricted-extras quicksynergy openjdk-7-jdk aspell-fr curl vim-gnome giggle bzr freeplane exuberant-ctags  mercurial remmina startupmanager   glipper &&\
+
 echo "Install Custom applications"
 sudo dpkg -i /home/sinarf/Dropbox/softs/linux/*.deb
 
-echo "Install software"
-sudo apt-get install -y -qq gawk maven2 groovy rar git git-gui openssh-server lm-sensors lame hddtemp gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla tasque monodevelop sun-java6-jdk sun-java6-plugin quicksynergy tomboy-blogposter playonlinux aspell-fr curl vim-gnome giggle bzr freeplane exuberant-ctags  thunderbird-gnome-support xul-ext-lightning mercurial remmina startupmanager rhythmbox-plugin-coherence  &&\
 
 sudo update-java-alternatives -s java-6-sun
