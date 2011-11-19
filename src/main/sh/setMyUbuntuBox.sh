e0713772a1048579cc1473b6b20365901e1a590b
Michel Blavin <michel@blavin.fr>
2011-11-19 15:42:58 +0100

    Added Dropbox configuration.

--- src/main/sh/setMyUbuntuBox.sh
+++ src/main/sh/setMyUbuntuBox.sh
@@ -4,7 +4,6 @@
 # This script sets up my ubuntu box  after a reinstallation. 
 #
 
-<<<<<<< HEAD
 zenity --question --text="Did you enable the partner repo and setup Ubuntu One? "
 if [ $? -eq 1 ] ; then 
 	zenity --info --text="Enable the partner repo first."
@@ -12,26 +11,11 @@ if [ $? -eq 1 ] ; then
 fi
 sudo ./autoUpdate.sh
 sudo update-java-alternatives -s java-1.7.0-openjdk-i386
-=======
-
-
-echo "Remove useless software"
-sudo apt-get -y -qq remove libreoffice firefox banshee &&\
-sudo apt-get -y -qq autoremove &&\
-
-echo "Update everything"
-sudo apt-get -y -qq update &&\
-sudo apt-get -y -qq upgrade &&\
-
-echo "Install software"
-sudo apt-get install -y -qq maven2 groovy rar git git-gui openssh-server lm-sensors lame hddtemp gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla tasque monodevelop sun-java6-jdk sun-java6-plugin quicksynergy gnome-do tomboy-blogposter playonlinux aspell-fr curl vim-gnome giggle docky bzr freeplane exuberant-ctags  thunderbird-gnome-support xul-ext-lightning mercurial &&\
-
-sudo update-java-alternatives -s java-6-sun
->>>>>>> 52ebfab1f5df193f484c9171bb229c1a6dda5f9e
+echo "Added some watch stuff for Dropbox."
+sudo sysctl fs.inotify.max_user_watches=100001
 echo "Correct the stupid choice to move the button to the left. "
 gconftool -s --type string /apps/metacity/general/button_layout ":minimize,maximize,close";
 
-# TODO add the possiblity to install thinkpad finger reader and configure it. 
 zenity --question --text="Do you want to install the thinkpad stuff? "
 if [ $? -eq 1 ] ; then
 	echo "thinkpad stuff won't be installed". 
@@ -41,8 +25,5 @@ else
 	sudo tf-tool --verify &&\
 	sudo /usr/lib/pam-thinkfinger/pam-thinkfinger-enable
 fi
-<<<<<<< HEAD
 # groovy stuff
 groovy ~/Dropbox/dev/groolbox/src/main/groovy/WorkstationSetup.groovy
-=======
->>>>>>> 52ebfab1f5df193f484c9171bb229c1a6dda5f9e
