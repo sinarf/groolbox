59d53b8eee30de89a085607eb9859ef9e9ff20db
Michel Blavin <michel@blavin.fr>
2011-12-05 15:07:29 +0100

    added gnome-do

--- src/main/sh/autoUpdate.sh
+++ src/main/sh/autoUpdate.sh
@@ -15,6 +15,6 @@ sudo apt-get -y -qq update &&\
 sudo apt-get -y -qq upgrade &&\
 
 echo "Install software"
-sudo apt-get install -y -qq gawk maven2 groovy rar git git-gui openssh-server lm-sensors lame gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla tasque  xubuntu-restricted-extras quicksynergy openjdk-7-jdk aspell-fr curl vim-gnome giggle bzr freeplane exuberant-ctags  mercurial remmina startupmanager   glipper libxml2-utils &&\
+sudo apt-get install -y -qq gnome gawk maven2 groovy rar git git-gui openssh-server lm-sensors lame gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla tasque  xubuntu-restricted-extras quicksynergy openjdk-7-jdk aspell-fr curl vim-gnome giggle bzr freeplane exuberant-ctags  mercurial remmina startupmanager   glipper libxml2-utils gradle gnome-do &&\
 
 
