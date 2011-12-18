813b7aef6024c4e2d7576a17d6783870546d27a7
Michel Blavin <michel@blavin.fr>
2011-12-18 13:24:57 +0100

    correct package name for rabbitvcs-nautilus plugin

--- src/main/sh/autoUpdate.sh
+++ src/main/sh/autoUpdate.sh
@@ -6,7 +6,7 @@
 #
 
 echo "Install software" 
-sudo apt-get install -y -qq gawk maven2 groovy rar git git-gui openssh-server lm-sensors lame gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla aspell-fr curl vim-gnome giggle bzr freeplane exuberant-ctags  mercurial remmina startupmanager giggle-personal-details-plugin glipper libxml2-utils gradle gitmagic rabbitvcs-nautilus
+sudo apt-get install -y -qq gawk maven2 groovy rar git git-gui openssh-server lm-sensors lame gpodder gparted gimp  chameleon-cursor-theme quicksynergy xsensors conduit screen subversion frozen-bubble wine hplip hplip-gui powertop compizconfig-settings-manager filezilla aspell-fr curl vim-gnome giggle bzr freeplane exuberant-ctags  mercurial remmina startupmanager giggle-personal-details-plugin glipper libxml2-utils gradle gitmagic rabbitvcs-nautilus3
 
 echo "Remove useless software: "
 sudo apt-get -y -qq remove libreoffice banshee &&\
