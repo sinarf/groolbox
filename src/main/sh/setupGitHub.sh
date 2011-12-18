e17d30d503a68532629166c1dbbb955ff844cc0b
Michel Blavin <michel@blavin.fr>
2011-12-18 13:17:31 +0100

    Added github ssh setup.

---
+++ src/main/sh/setupGitHub.sh
@@ -0,0 +1,7 @@
+cd ~/ssh/
+ssh-keygen -t rsa -C "michel@blavin.fr"
+echo "Goto https://github.com/account/ssh and paste the following "
+echo "********************************************************************************"
+cat ~/.ssh/id_rsa.pub
+echo "********************************************************************************"
+
