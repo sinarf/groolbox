e4b181dd2d87d692f9ac06f6e6b80f4548ed2d4e
Michel Blavin <michel@blavin.fr>
2011-12-18 13:38:32 +0100

    opens the chrome automaticaly

--- src/main/sh/setupGitHub.sh
+++ src/main/sh/setupGitHub.sh
@@ -4,4 +4,4 @@ echo "Goto https://github.com/account/ssh and paste the following "
 echo "********************************************************************************"
 cat ~/.ssh/id_rsa.pub
 echo "********************************************************************************"
-
+google-chrome https://github.com/account/ssh
