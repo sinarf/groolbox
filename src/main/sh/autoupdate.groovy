b6a47787eeb4708fe5e27c1b51140fa9555d0183
Michel Blavin <michel@blavin.fr>
2012-01-15 12:04:01 +0100

    removed obsolete files

--- src/main/sh/autoupdate.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/groovy
-
-new File("applications.txt").eachLine { line ->
-	if (!line.startsWith("#")){
-		println  "installing $line"
-		println "sudo apt-get install -yy $line".execute().text
-	}
-}
